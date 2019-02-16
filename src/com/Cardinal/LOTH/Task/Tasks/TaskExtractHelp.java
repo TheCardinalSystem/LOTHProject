27cca40596565845306cb6f1b625b83ece8b4078
TheCardinalSystem <dudehenry.peace@gmail.com>
2019-02-15 21:06:14 -0500

    Fixed more bugs
    java.util.zip refused to work. It was destroying the PNGs. Had to swap over to Zip4J

--- src/com/Cardinal/LOTH/Task/Tasks/TaskExtractHelp.java
+++ src/com/Cardinal/LOTH/Task/Tasks/TaskExtractHelp.java
@@ -2,22 +2,19 @@ package com.Cardinal.LOTH.Task.Tasks;
 
 import java.awt.Desktop;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 import javax.swing.JFrame;
 
 import com.Cardinal.LOTH.WorkspaceConstants;
 import com.Cardinal.LOTH.Gui.Frames.ErrorFrame;
 import com.Cardinal.LOTH.Task.ITask;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 public class TaskExtractHelp implements ITask {
 
@@ -29,7 +26,7 @@ public class TaskExtractHelp implements ITask {
 
 	@Override
 	public ITask[] runTask() {
-		if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+		if (System.getProperty("os.name").toLowerCase().indexOf("win") < 0) {
 			try {
 				File f = new File(WorkspaceConstants.HELPDIRECTORY, "LOTHProject.chm");
 
@@ -37,7 +34,7 @@ public class TaskExtractHelp implements ITask {
 
 					if (!f.getParentFile().exists())
 						f.getParentFile().mkdirs();
-					
+
 					Files.copy(ClassLoader.getSystemResourceAsStream("assets/Help/LOTHHelp.chm"), f.toPath());
 
 				}
@@ -51,34 +48,16 @@ public class TaskExtractHelp implements ITask {
 			}
 		} else {
 			try {
-				ZipFile zip = new ZipFile(new File(ClassLoader.getSystemResource("assets/Help/Help.zip").toURI()));
+				File f = new File(WorkspaceConstants.HELPDIRECTORY, "Help.zip");
+				Files.copy(ClassLoader.getSystemResourceAsStream("assets/Help/Help.zip"), f.toPath());
 
-				ZipEntry entry;
+				ZipFile zip = new ZipFile(f);
 
-				Enumeration<? extends ZipEntry> entries = zip.entries();
-				while (entries.hasMoreElements()) {
-					entry = entries.nextElement();
-					File file = new File(WorkspaceConstants.HELPDIRECTORY, entry.getName());
+				zip.extractAll(f.getParent());
 
-					if (!file.exists() || file.length() < 0) {
-						if (!file.getParentFile().exists())
-							file.getParentFile().mkdirs();
-
-						InputStream entIn = zip.getInputStream(entry);
-						FileOutputStream out = new FileOutputStream(file);
-						byte[] buffer = new byte[2048];
-						@SuppressWarnings("unused")
-						int i;
-						while ((i = entIn.read(buffer)) > 0) {
-							out.write(buffer);
-						}
-						out.flush();
-						out.close();
-					}
-				}
-				zip.close();
+				f.delete();
 				Desktop.getDesktop().browse(new File(WorkspaceConstants.HELPDIRECTORY, "Welcome.html").toURI());
-			} catch (IOException | URISyntaxException e) {
+			} catch (IOException | ZipException e) {
 				StringWriter sw = new StringWriter();
 				PrintWriter pw = new PrintWriter(sw);
 				e.printStackTrace(pw);
